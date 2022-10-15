7cae56724688df18e90630015e02ee3c6b5c2a95
Macfros <90770458+Macfros@users.noreply.github.com>
2022-10-15 18:55:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+const express=require("express");
+const https=require("https");
+const bodyParser=require("body-parser");
+const _=require("lodash");
+const ejs=require("ejs");
+const app=express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(express.static("public"));
+
+
+app.get("/",function(req,res){
+res.sendFile(__dirname +"\\index.html");
+})
+
+app.post("/",function(req,res){
+  const city=req.body.CityName;
+
+  const url="https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&appid=c6d442adf9595680af9098f5a91497a4";
+  https.get(url,function(response){
+
+    response.on("data",function(data){
+
+      const weather_data=JSON.parse(data);
+      const des=weather_data.weather[0].description;
+      const temp=weather_data.main.temp;
+      const icon=weather_data.weather[0].icon;
+      const imageURL=" http://openweathermap.org/img/wn/"+ icon +"@2x.png";
+
+
+        res.write("<p>Temperature of "+_.capitalize(req.body.CityName)+" is "+ temp +" degrees with  "+ des+"</p>");
+         res.write("<img src=" + imageURL + ">");
+
+        res.send();
+
+    })
+
+  })
+
+})
+
+
+app.listen(3000,function(){
+
+  console.log("Server is running on port 3000");
+})
